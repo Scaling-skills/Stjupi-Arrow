42d408eca25fe1e893296e8aabb4aa2b0af5d651
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-08 21:36:32 +0100

    Collision update
    Destroy object, when colide with player

--- Stjupi Arrow/Assets/Collision.cs
+++ Stjupi Arrow/Assets/Collision.cs
@@ -6,7 +6,7 @@ public class Collision : MonoBehaviour
 {
      private void OnCollisionEnter2D(Collision2D other) 
     {
-        Debug.Log("Hitti");
+        Destroy(other.gameObject);
     }
     
 }

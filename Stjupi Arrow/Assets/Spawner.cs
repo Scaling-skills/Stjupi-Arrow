d02d9307d08cda43a4794c43a171dd0300ca907f
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-09 21:58:01 +0100

    Spawner
    Spawner update

--- Stjupi Arrow/Assets/Spawner.cs
+++ Stjupi Arrow/Assets/Spawner.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
     public GameObject FruitPrefab;
     public float respawnTime = 1.0f;
-
+    
     // Use this for initialization
     void Start () {
         StartCoroutine(FruitSpawner());
     }
     private void spawnEnemy(){
         GameObject a = Instantiate(FruitPrefab) as GameObject;
-        a.transform.position = new Vector3(Random.Range(-2, 2), 10, -55);
+        a.transform.position = new Vector3(Random.Range(-3, 3), 10, -55);
     }
     IEnumerator FruitSpawner(){
         while(true){

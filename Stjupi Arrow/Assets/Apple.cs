4e9d42d895660ab44de63cd26189ab9a72c46ad5
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-10 14:02:29 +0100

    -

--- Stjupi Arrow/Assets/Apple.cs
+++ Stjupi Arrow/Assets/Apple.cs
@@ -7,15 +7,22 @@ public class Apple : MonoBehaviour
     public float speed = 10.0f;
     private Rigidbody2D rb;
     private Vector2 screenBounds;
-
+    
 
     // Use this for initialization
     void Start () 
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector3(0,-speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.y));
 
     }
-
-    // Update is called once per frame
+ // Update is called once per frame
+    void Update () 
+    {
+        if(transform.position.y < screenBounds.y * -1.2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Stjupi Arrow/Assets/Spawner.cs
+++ Stjupi Arrow/Assets/Spawner.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+ {
     public GameObject FruitPrefab;
     public float respawnTime = 1.0f;
     
@@ -10,12 +11,15 @@ public class Spawner : MonoBehaviour {
     void Start () {
         StartCoroutine(FruitSpawner());
     }
-    private void spawnEnemy(){
+    private void spawnEnemy()
+    {
         GameObject a = Instantiate(FruitPrefab) as GameObject;
         a.transform.position = new Vector3(Random.Range(-3, 3), 10, -55);
     }
-    IEnumerator FruitSpawner(){
-        while(true){
+    IEnumerator FruitSpawner()
+    {
+        while(true)
+        {
             yield return new WaitForSeconds(respawnTime);
             spawnEnemy();
         }

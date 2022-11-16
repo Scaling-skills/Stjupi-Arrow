4e956ba0fefd62ca7679263d3ba5e03ec40e6818
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-16 21:51:45 +0100

    Sounds

--- Stjupi Arrow/Assets/Assets for my project/Scripts/Collision.cs
+++ Stjupi Arrow/Assets/Assets for my project/Scripts/Collision.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collision : MonoBehaviour 
+
 {
+public AudioSource AppleBite;
      private void OnCollisionEnter2D(Collision2D other) 
     {
+        AppleBite = GetComponent<AudioSource> ();
         Destroy(other.gameObject);
         Score.scoreValue += 1;
+        AppleBite.Play();
     }
 }
+ 

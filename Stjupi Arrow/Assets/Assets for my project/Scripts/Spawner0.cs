b6d245b63ab845c68b0f5670a6d5414733a09400
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-25 19:52:09 +0100

    -

---
+++ Stjupi Arrow/Assets/Assets for my project/Scripts/Spawner0.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner0 : MonoBehaviour
+ {
+    public GameObject FruitPrefab;
+    public float respawnTime = 1.0f;
+    
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FruitSpawner());
+    }
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(FruitPrefab) as GameObject;
+        a.transform.position = new Vector3(Random.Range(-3, 3), 10, -1);
+    }
+    IEnumerator FruitSpawner()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}

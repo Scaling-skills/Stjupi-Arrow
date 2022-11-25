4dcf11a99af1160394e53f87f30ca2efcbb62b25
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-25 18:55:28 +0100

    Teleport

---
+++ Stjupi Arrow/Assets/PlayerTeleport.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
---
+++ Stjupi Arrow/Assets/Teleporter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+
+
+
+
+
+
+}
+
+

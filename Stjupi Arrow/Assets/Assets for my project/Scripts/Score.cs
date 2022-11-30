0b1427b144182ea29a0d21d5d83e70e9233eee84
Scaling-skills <a.adam.wodzinski@gmail.com>
2022-11-30 17:18:45 +0100

    -

--- Stjupi Arrow/Assets/Assets for my project/Scripts/Score.cs
+++ Stjupi Arrow/Assets/Assets for my project/Scripts/Score.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+
     public static int scoreValue = 0;
     Text score;
-    
+    [SerializeField] Text highScoreText;
     void Start()
     {
         score = GetComponent<Text> ();
@@ -17,5 +18,18 @@ public class Score : MonoBehaviour
     void Update()
     {
         score.text = "" + scoreValue;
+        CheckHighScore();
+    }
+    void CheckHighScore()
+    {
+        if(scoreValue > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", scoreValue);
+            UpadateHighScoreText();
+        }
+    }
+    void UpadateHighScoreText()
+    {
+        highScoreText.text = $"HighScore: {PlayerPrefs.GetInt("HighScore", 0)}";
     }
 }
